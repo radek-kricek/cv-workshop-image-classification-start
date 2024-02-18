30148071bbeb47ddf2656debaba38f7306e41e0c
radek-kricek <radek.kricek@gmail.com>
2024-02-18 19:03:59 +0100

    version from onsite

---
+++ constants.py
@@ -0,0 +1,6 @@
+max_images_per_label = 500
+#data_set_folder = "dogs-cats"
+data_set_folder = "rice-image-dataset-small/rice-image-dataset-small"
+data_set_path = f"data/{data_set_folder}/"
+image_size = (160, 160)
+# image_size = (96, 96)
---
+++ utils.py
@@ -0,0 +1,7 @@
+def encode_labels(df, label_mapping):
+    df["encode_label"] = df["label"].map(label_mapping)
+    return df
+
+
+def map_labels(labels):
+    return {label: idx for idx, label in enumerate(labels)}
